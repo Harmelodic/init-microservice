20c904cd5b49fae103f816ba662f6ef2aaf3fa7f
Matt Smith <matt@harmelodic.com>
2022-07-27 20:29:19 +0200

    Using preferred LocalServerPort class

--- src/test/java/com/harmelodic/init/microservice/account/AccountControllerTest.java
+++ src/test/java/com/harmelodic/init/microservice/account/AccountControllerTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
